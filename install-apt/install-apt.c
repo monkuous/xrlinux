74f8709ef97c7a906090537959fff57227bd11c1
monkuous <84319635+monkuous@users.noreply.github.com>
2025-09-01 15:21:27 +0200

    install-apt: change structure naming style

--- install-apt/install-apt.c
+++ install-apt/install-apt.c
@@ -28,49 +28,49 @@
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
 
-typedef struct {
-    uint8_t label[8];
-    uint32_t sectorCount;
-    uint32_t status;
-} __attribute__((packed)) apt_entry_t;
+struct AptEntry {
+    uint8_t Label[8];
+    uint32_t SectorCount;
+    uint32_t Status;
+} __attribute__((packed));
 
-typedef struct {
-    uint8_t bootCode[15];
-    uint8_t ffIfVariant;
-    apt_entry_t partitions[8];
-    uint32_t magic;
-    uint8_t label[16];
-} __attribute__((packed)) apt_boot_block_t;
+struct AptBootBlock {
+    uint8_t BootCode[15];
+    uint8_t FfIfVariant;
+    struct AptEntry Partitions[8];
+    uint32_t Magic;
+    uint8_t Label[16];
+} __attribute__((packed));
 
-typedef struct {
-    uint32_t magic;
-    uint8_t osName[16];
-    uint32_t bootstrapSector;
-    uint32_t bootstrapCount;
-} __attribute__((packed)) apt_os_record_t;
+struct AptOsRecord {
+    uint32_t Magic;
+    uint8_t OsName[16];
+    uint32_t BootstrapSector;
+    uint32_t BootstrapCount;
+} __attribute__((packed));
 
-typedef struct {
-    uint8_t bootIndicator;
-    uint8_t startingChs[3];
-    uint8_t osType;
-    uint8_t endingChs[3];
-    uint32_t startingLba;
-    uint32_t sizeInLba;
-} __attribute__((packed)) mbr_part_t;
+struct MbrPartition {
+    uint8_t BootIndicator;
+    uint8_t StartingChs[3];
+    uint8_t OsType;
+    uint8_t EndingChs[3];
+    uint32_t StartingLba;
+    uint32_t SizeInLba;
+} __attribute__((packed));
 
-typedef struct {
+struct Mbr {
     union {
-        uint8_t bootCode[424];
-        apt_boot_block_t apt;
+        uint8_t BootCode[424];
+        struct AptBootBlock Apt;
     };
-    uint8_t reserved[16];
-    uint32_t uniqueMbrDiskSignature;
-    uint16_t unknown;
-    mbr_part_t partitionRecord[4];
-    uint16_t signature;
-} __attribute__((packed)) mbr_t;
+    uint8_t Reserved[16];
+    uint32_t UniqueMbrDiskSignature;
+    uint16_t Unknown;
+    struct MbrPartition PartitionRecord[4];
+    uint16_t Signature;
+} __attribute__((packed));
 
-_Static_assert(sizeof(mbr_t) == 512, "Invalid size for mbr_t");
+_Static_assert(sizeof(struct Mbr) == 512, "Invalid size for struct Mbr");
 
 static const char *AppName;
 static const uint8_t OsLogo[512];
@@ -135,17 +135,17 @@ int main(int argc, char *argv[]) {
     int disk = open(argv[1], O_RDWR);
     if (disk < 0) PerrorDie("failed to open disk");
 
-    mbr_t mbr;
+    struct Mbr mbr;
     ReadFully(disk, &mbr, sizeof(mbr), 0);
-    if (LE16(mbr.signature) != MBR_SIGNATURE) ErrorDie("invalid mbr");
+    if (LE16(mbr.Signature) != MBR_SIGNATURE) ErrorDie("invalid mbr");
 
     uint32_t aptPartitionStart = APT_START_SECTOR;
     uint32_t aptPartitionEnd = DEFAULT_AVAIL_SIZE / SECTOR_SIZE;
 
-    for (int i = 0; i < ARRAY_SIZE(mbr.partitionRecord); i++) {
-        mbr_part_t *part = &mbr.partitionRecord[i];
-        if (part->osType == 0 || part->sizeInLba == 0) continue;
-        if (part->startingLba < aptPartitionEnd) aptPartitionEnd = part->startingLba;
+    for (int i = 0; i < ARRAY_SIZE(mbr.PartitionRecord); i++) {
+        struct MbrPartition *part = &mbr.PartitionRecord[i];
+        if (part->OsType == 0 || part->SizeInLba == 0) continue;
+        if (part->StartingLba < aptPartitionEnd) aptPartitionEnd = part->StartingLba;
     }
 
     if (aptPartitionEnd <= aptPartitionStart) ErrorDie("no space on disk for apt");
@@ -166,23 +166,23 @@ int main(int argc, char *argv[]) {
     CopyData(image, disk, st.st_size, 0, (aptPartitionStart + APT_BOOT_SECTORS) * SECTOR_SIZE);
 
     // Write image header to disk
-    apt_os_record_t osRecord = {
-        .magic = APT_BOOT_MAGIC,
-        .bootstrapSector = APT_BOOT_SECTORS,
-        .bootstrapCount = imageSectors,
+    struct AptOsRecord osRecord = {
+        .Magic = APT_BOOT_MAGIC,
+        .BootstrapSector = APT_BOOT_SECTORS,
+        .BootstrapCount = imageSectors,
     };
-    memcpy(osRecord.osName, name, strlen(name));
+    memcpy(osRecord.OsName, name, strlen(name));
 
     WriteFully(disk, &osRecord, sizeof(osRecord), (aptPartitionStart + 1) * SECTOR_SIZE);
     WriteFully(disk, OsLogo, sizeof(OsLogo), (aptPartitionStart + 2) * SECTOR_SIZE);
 
     // Finally, create the actual partition table and write it to disk
     // This is done last so that the effects of the previous steps are invisible unless everything is successful
-    memset(&mbr.apt, 0, sizeof(mbr.apt));
-    mbr.apt.ffIfVariant = 0xff;
-    mbr.apt.magic = APT_MAGIC;
-    mbr.apt.partitions[0].sectorCount = aptPartitionAvailable;
-    mbr.apt.partitions[0].status = 1;
+    memset(&mbr.Apt, 0, sizeof(mbr.Apt));
+    mbr.Apt.FfIfVariant = 0xff;
+    mbr.Apt.Magic = APT_MAGIC;
+    mbr.Apt.Partitions[0].SectorCount = aptPartitionAvailable;
+    mbr.Apt.Partitions[0].Status = 1;
     WriteFully(disk, &mbr, sizeof(mbr), 0);
 
     return 0;
